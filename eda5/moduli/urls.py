b545c35a9b804050f8bcd0ea7053b0eff05f68a7
Vasja <vasja.pavlovic@gmail.com>
2015-10-26 06:36:33 +0100

    Izdelava aplikacije računovodstvo

--- eda5/moduli/urls.py
+++ eda5/moduli/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     # registracija MODULOV (urlji)
     url(r'^partnerji/', include("eda5.partnerji.urls", namespace="partnerji")),
     url(r'^posta/', include("eda5.posta.urls", namespace="posta")),
+    url(r'^racunovodstvo/', include("eda5.racunovodstvo.urls", namespace="racunovodstvo")),
     url(r'^stores/', include("eda5.stores.urls", namespace="stores")),
 ]
 
--- eda5/racunovodstvo/views.py
+++ eda5/racunovodstvo/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import TemplateView
+
+
+class RacunovodstvoHomeView(TemplateView):
+    template_name = "racunovodstvo/home.html"
