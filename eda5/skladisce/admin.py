7ba2bbe9bd6466511d38e7ce23695f5f1fed26d7
Vasja <vasja.pavlovic@gmail.com>
2015-11-17 19:15:16 +0100

    pred migracijo aplikacije skladisce + admin

--- eda5/skladisce/admin.py
+++ eda5/skladisce/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Dobava, Artikel, TipArtikla, SklopArtikla, Dnevnik
+
+
+@admin.register(Dobava)
+class DobavaAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Artikel)
+class ArtikelAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(TipArtikla)
+class TipArtiklaAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(SklopArtikla)
+class SklopArtiklaAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Dnevnik)
+class DnevnikAdmin(admin.ModelAdmin):
+    pass
