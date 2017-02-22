56aeae406198aa8cdd6be78122c963b8fd37b238
Vasja Pavlovič <vasja.pavlovic@gmail.com>
2017-02-22 07:29:14 -0800

    popravki s tomažem

--- config/settings/production.py
+++ config/settings/production.py
@@ -119,6 +119,7 @@ SERVER_EMAIL = env('DJANGO_SERVER_EMAIL', default=DEFAULT_FROM_EMAIL)
 # ------------------------------------------------------------------------------
 # See:
 # https://docs.djangoproject.com/en/dev/ref/templates/api/#django.template.loaders.cached.Loader
+TEMPLATES[0]['OPTIONS']['debug'] = DEBUG  # popraviti
 # TEMPLATES[0]['OPTIONS']['loaders'] = [
 #     ('django.template.loaders.cached.Loader', [
 #         'django.template.loaders.filesystem.Loader', 'django.template.loaders.app_directories.Loader', ]),
