c3a4ddac70f8b63734fd53484c8727091cd75ae9
Vasja Pavlovič <vasja.pavlovic@gmail.com>
2017-05-14 01:43:10 -0700

    Redni backup 14.5.2017

--- eda5/reports/forms.py
+++ eda5/reports/forms.py
@@ -1,9 +1,19 @@
+# Python
+# Django
 from django import forms
 from django.utils.html import conditional_escape, mark_safe
 from django.utils.encoding import smart_text
 
-from eda5.racunovodstvo.models import SkupinaVrsteStroska
+# Models
+from eda5.core.models import ObdobjeLeto, ObdobjeMesec
 from eda5.etaznalastnina.models import Program
+from eda5.racunovodstvo.models import SkupinaVrsteStroska
+
+
+
+# Forms
+
+
 
 
 class ReportForm(forms.Form):
@@ -29,3 +39,12 @@ class DeliSeznamFilterForm(forms.Form):
 
     program = forms.ModelMultipleChoiceField(
         queryset=Program.objects.all())
+
+
+class ObracunFilterForm(forms.Form):
+
+    obdobje_leto = forms.ModelChoiceField(
+        queryset=ObdobjeLeto.objects.all())
+
+    obdobje_mesec = forms.ModelChoiceField(
+        queryset=ObdobjeMesec.objects.all())
