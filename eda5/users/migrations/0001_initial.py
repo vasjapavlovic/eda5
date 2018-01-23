9c8c871794622563751c1db5eb6a7eed9dab354b
Vasja Pavlovič <vasja.pavlovic@gmail.com>
2018-01-22 22:29:46 -0800

    čista OSNOVNA migracija

--- eda5/users/migrations/0001_initial.py
+++ eda5/users/migrations/0001_initial.py
@@ -2,9 +2,9 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
-import django.core.validators
 import django.contrib.auth.models
 import django.utils.timezone
+import django.core.validators
 
 
 class Migration(migrations.Migration):
@@ -17,25 +17,25 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='User',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
-                ('password', models.CharField(verbose_name='password', max_length=128)),
-                ('last_login', models.DateTimeField(verbose_name='last login', null=True, blank=True)),
-                ('is_superuser', models.BooleanField(verbose_name='superuser status', default=False, help_text='Designates that this user has all permissions without explicitly assigning them.')),
-                ('username', models.CharField(max_length=30, help_text='Required. 30 characters or fewer. Letters, digits and @/./+/-/_ only.', error_messages={'unique': 'A user with that username already exists.'}, verbose_name='username', unique=True, validators=[django.core.validators.RegexValidator('^[\\w.@+-]+$', 'Enter a valid username. This value may contain only letters, numbers and @/./+/-/_ characters.', 'invalid')])),
-                ('first_name', models.CharField(verbose_name='first name', max_length=30, blank=True)),
-                ('last_name', models.CharField(verbose_name='last name', max_length=30, blank=True)),
-                ('email', models.EmailField(verbose_name='email address', max_length=254, blank=True)),
-                ('is_staff', models.BooleanField(verbose_name='staff status', default=False, help_text='Designates whether the user can log into this admin site.')),
-                ('is_active', models.BooleanField(verbose_name='active', default=True, help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.')),
-                ('date_joined', models.DateTimeField(verbose_name='date joined', default=django.utils.timezone.now)),
-                ('name', models.CharField(verbose_name='Name of User', max_length=255, blank=True)),
-                ('groups', models.ManyToManyField(help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', blank=True, verbose_name='groups', related_query_name='user', to='auth.Group', related_name='user_set')),
-                ('user_permissions', models.ManyToManyField(help_text='Specific permissions for this user.', blank=True, verbose_name='user permissions', related_query_name='user', to='auth.Permission', related_name='user_set')),
+                ('id', models.AutoField(auto_created=True, verbose_name='ID', primary_key=True, serialize=False)),
+                ('password', models.CharField(max_length=128, verbose_name='password')),
+                ('last_login', models.DateTimeField(null=True, blank=True, verbose_name='last login')),
+                ('is_superuser', models.BooleanField(default=False, help_text='Designates that this user has all permissions without explicitly assigning them.', verbose_name='superuser status')),
+                ('username', models.CharField(error_messages={'unique': 'A user with that username already exists.'}, unique=True, verbose_name='username', max_length=30, validators=[django.core.validators.RegexValidator('^[\\w.@+-]+$', 'Enter a valid username. This value may contain only letters, numbers and @/./+/-/_ characters.', 'invalid')], help_text='Required. 30 characters or fewer. Letters, digits and @/./+/-/_ only.')),
+                ('first_name', models.CharField(max_length=30, blank=True, verbose_name='first name')),
+                ('last_name', models.CharField(max_length=30, blank=True, verbose_name='last name')),
+                ('email', models.EmailField(max_length=254, blank=True, verbose_name='email address')),
+                ('is_staff', models.BooleanField(default=False, help_text='Designates whether the user can log into this admin site.', verbose_name='staff status')),
+                ('is_active', models.BooleanField(default=True, help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.', verbose_name='active')),
+                ('date_joined', models.DateTimeField(default=django.utils.timezone.now, verbose_name='date joined')),
+                ('name', models.CharField(max_length=255, blank=True, verbose_name='Name of User')),
+                ('groups', models.ManyToManyField(to='auth.Group', verbose_name='groups', related_query_name='user', related_name='user_set', blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.')),
+                ('user_permissions', models.ManyToManyField(to='auth.Permission', verbose_name='user permissions', related_query_name='user', related_name='user_set', blank=True, help_text='Specific permissions for this user.')),
             ],
             options={
-                'verbose_name': 'user',
                 'abstract': False,
                 'verbose_name_plural': 'users',
+                'verbose_name': 'user',
             },
             managers=[
                 ('objects', django.contrib.auth.models.UserManager()),
---
+++ initial_setup.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+cp eda5/contrib/sites/inital_backup/0001_initial.py eda5/contrib/sites/migrations/0001_initial.py
